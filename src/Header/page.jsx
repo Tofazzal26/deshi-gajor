a8b5672ee409d644b6daf1c1ec590ceb079b0578
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-05-12 12:17:43 +0600

    not found page style finish

--- src/Header/page.jsx
+++ src/Header/page.jsx
@@ -1,51 +1,118 @@
+"use client";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
-
+import Link from "next/link";
 const Header = () => {
+  const router = useRouter();
+  const path = usePathname();
   return (
     <div className="bg-[#03211b] text-white py-2 border-b-[1px] border-[#1c3020]">
       <div className="container mx-auto">
         <div>
           <div className="navbar">
             <div className="navbar-start">
-              <button className="flex items-center gap-1 text-[22px] lg:text-[32px]">
-                <Image
-                  className="rotate-[40deg] lg:w-[65px] lg:h-[65px] w-[50px] h-[50px]"
-                  src="/logoThree.png"
-                  width={65}
-                  height={65}
-                  alt="logo"
-                />{" "}
-                <h2>
-                  {" "}
-                  Deshi <span className="text-[#ffb25a]"> Gajor</span>
-                </h2>
-              </button>
+              <Link href="/">
+                <button className="flex items-center gap-1 text-[22px] cursor-pointer lg:text-[32px]">
+                  <Image
+                    className="rotate-[40deg] lg:w-[65px] lg:h-[65px] w-[50px] h-[50px]"
+                    src="/logoThree.png"
+                    width={65}
+                    height={65}
+                    alt="logo"
+                  />{" "}
+                  <h2>
+                    {" "}
+                    Deshi <span className="text-[#ffb25a]"> Gajor</span>
+                  </h2>
+                </button>
+              </Link>
             </div>
             <div className="navbar-end">
               <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 items-center gap-4">
                   <li>
-                    <h2 className="text-[17px]">Home</h2>
+                    <h2 className="text-[17px]">
+                      {" "}
+                      <Link
+                        href="/"
+                        className={path === "/" ? "text-[#ffb25a]" : ""}
+                      >
+                        Home
+                      </Link>{" "}
+                    </h2>
                   </li>
                   <li>
-                    <h2 className="text-[17px]">Review</h2>
+                    <h2 className="text-[17px]">
+                      <Link
+                        href="/api/review"
+                        className={
+                          path === "/api/review" ? "text-[#ffb25a]" : ""
+                        }
+                      >
+                        Review
+                      </Link>
+                    </h2>
                   </li>
                   <li>
-                    <h2 className="text-[17px]">Israel Product</h2>
+                    <h2 className="text-[17px]">
+                      <Link
+                        href="/api/israel"
+                        className={
+                          path === "/api/israel" ? "text-[#ffb25a]" : ""
+                        }
+                      >
+                        Israel Product
+                      </Link>
+                    </h2>
                   </li>
                   <li>
-                    <h2 className="text-[17px]">Blog</h2>
+                    <h2 className="text-[17px]">
+                      {" "}
+                      <Link
+                        href="/api/blog"
+                        className={path === "/api/blog" ? "text-[#ffb25a]" : ""}
+                      >
+                        Blog
+                      </Link>
+                    </h2>
                   </li>
                   <li>
-                    <h2 className="text-[17px]">About</h2>
+                    <h2 className="text-[17px]">
+                      {" "}
+                      <Link
+                        href="/api/about"
+                        className={
+                          path === "/api/about" ? "text-[#ffb25a]" : ""
+                        }
+                      >
+                        About
+                      </Link>
+                    </h2>
                   </li>
                   <li>
-                    <h2 className="text-[17px]">Contact</h2>
+                    <h2 className="text-[17px]">
+                      {" "}
+                      <Link
+                        href="/api/contact"
+                        className={
+                          path === "/api/contact" ? "text-[#ffb25a]" : ""
+                        }
+                      >
+                        Contact
+                      </Link>
+                    </h2>
                   </li>
                   <li>
                     <button className="bg-[#074c3e] text-[17px] px-5 py-2 rounded-full">
-                      Login
+                      <Link
+                        href="/api/login"
+                        className={
+                          path === "/api/login" ? "text-[#ffb25a]" : ""
+                        }
+                      >
+                        Login
+                      </Link>
                     </button>
                   </li>
                 </ul>
