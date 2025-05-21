f789c663295d42df449b919a624999ef4df79ca5
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-05-22 01:33:09 +0600

    footer and banner some style change

--- src/Components/Banner/page.jsx
+++ src/Components/Banner/page.jsx
@@ -11,12 +11,14 @@ const Banner = () => {
     <div className="bg-[#03211b] text-white">
       <div className="container mx-auto lg:px-0 px-2">
         <div className="pt-[50px]">
-          <div className="flex lg:gap-0 gap-6 lg:flex-row md:flex-row flex-col items-center justify-between">
-            <div>
-              <h2 className="text-base lg:text-xl text-[#ffb25a] flex items-center gap-4">
-                <Image src="/title.png" width={30} height={30} alt="title" />
-                Share Your Experience, Help Others Decide
-              </h2>
+          <div className="flex lg:gap-0 gap-6 lg:flex-row md:flex-row flex-col items-center justify-center md:justify-between lg:justify-between">
+            <div className="lg:text-start md:text-start text-center">
+              <div className="flex lg:justify-start md:justify-start justify-center items-center">
+                <h2 className="text-base lg:text-xl text-[#ffb25a] flex items-center gap-4">
+                  <Image src="/title.png" width={30} height={30} alt="title" />
+                  Share Your Experience, Help Others Decide
+                </h2>
+              </div>
               <h2 className="text-xl lg:text-[64px] lg:w-[550px] lg:leading-[75px] my-3 lg:my-4">
                 Know the Company Before You Trust It
               </h2>
@@ -25,7 +27,7 @@ const Banner = () => {
                 confusing. That’s why we created a platform where real people
                 share real experiences — helping you make smarter decisions.
               </p>
-              <div className="flex items-center gap-3 lg:gap-6 mt-3 lg:mt-7">
+              <div className="flex items-center lg:justify-start md:justify-start justify-center gap-3 lg:gap-6 mt-3 lg:mt-7">
                 <button
                   onClick={() => router.push(`/api/review`)}
                   className="flex items-center gap-2 rounded-full px-6 lg:px-8 py-[12px] lg:py-[14px] cursor-pointer  bg-[#074c3e] "
